36759f9ce77974b59fad3febb64cd3f99678498b
Sig Mouse <iam@sigmouse.com>
2024-11-08 22:19:53 -0700

    got gems falling

--- scenes/paddle.gd
+++ scenes/paddle.gd
@@ -1,10 +1,6 @@
 extends Area2D
 
-class_name Gem
-
-signal on_gem_off_screen
-
-@export var speed:float = 100.0
+@export var speed: float = 60.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,9 +9,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y += speed * delta
-	
-	if position.y > get_viewport_rect().size.y:
-		on_gem_off_screen.emit()
-		set_process(false)
-		queue_free()
+	if Input.is_action_pressed("left"):
+		position.x += -speed * delta
+		
+	if Input.is_action_pressed("right"):
+		position.x += speed * delta
